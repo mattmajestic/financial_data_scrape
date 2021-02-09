9bbed40bb1320bfefe548a6dd33498f17aaec4ac
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2021-02-08 17:09:50 -0700

    commented the t rowe price example

--- global.R
+++ global.R
@@ -1,4 +1,5 @@
-### T Rowe Price API Data ###
+### Loading various libraries ###
+## These are packages (pre compiled code) we will need to start working on getting, manipulating, and viz data ##
 
 library(httr)
 library(XML)
@@ -13,18 +14,34 @@ library(shinydashboard)
 library(shinyWidgets)
 library(BatchGetSymbols)
 library(plotly)
-# library(shinyParallel)
 
+## T Rowe Price Address to perform API request ##
 t_rowe_base_url <- "https://www3.troweprice.com/fb2/ppfweb/dailyPrices.do?cat="
+## T Rowe Price Metrics to query ##
 t_rowe_price_tabs <- c("MM","DOMSTOCK","INTSTOCK","TXBOND","TXFRBOND")
 
+## Create a chararacter with each word being the specific address ##
+## Create an empty chacter vector to store values of for loop ##
 t_rowe_price_urls <- as.character()
+
+## Run the for loop ##
 for(tab in 1:length(t_rowe_price_tabs)){
   t_rowe_price_urls <- c(t_rowe_price_urls,paste0(t_rowe_base_url,t_rowe_price_tabs[tab]))
 }
 
+## From this loop you can see the output below:
+# [1] "https://www3.troweprice.com/fb2/ppfweb/dailyPrices.do?cat=MM"      
+# [2] "https://www3.troweprice.com/fb2/ppfweb/dailyPrices.do?cat=DOMSTOCK"
+# [3] "https://www3.troweprice.com/fb2/ppfweb/dailyPrices.do?cat=INTSTOCK"
+# [4] "https://www3.troweprice.com/fb2/ppfweb/dailyPrices.do?cat=TXBOND"  
+# [5] "https://www3.troweprice.com/fb2/ppfweb/dailyPrices.do?cat=TXFRBOND"
+
+## Notice it is just combining line 19 and 21 content ##
+
+## Create an empty list to store data sets from T Rowe Price Website ##
 t_rowe_price_data <- list()
 
+## Run the for loop ##
 for(t_rowe_tab in 1:length(t_rowe_price_tabs)){
   t_rowe_html_table <- read_html(t_rowe_price_urls[t_rowe_tab], as.data.frame=T, stringsAsFactors = TRUE)
   x <- t_rowe_html_table %>%  
